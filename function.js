8e7ca208b5ab981d03fee75aa01594a26075988d
Sylo <133998903+Khyn2724@users.noreply.github.com>
2024-04-16 04:24:40 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+function openWindow() {
+    window.open("https://khyn2724.github.io/SignIn/");
+   }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function openRegister() {
+    window.open("https://www.sti.edu/admissions_registration.asp");
+   }
